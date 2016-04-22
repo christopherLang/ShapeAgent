2103e320408108e476fafe7922f7c346610219c2
Christopher Lang <chlang206@gmail.com>
2016-04-22 00:33:40 -0400

    Made alterations to TestLibrary. Now does this mosquito fly example to test performance

--- src/shapeAgent/TestLibrary.java
+++ src/shapeAgent/TestLibrary.java
@@ -6,68 +6,69 @@ import processing.core.*;
 public class TestLibrary extends PApplet {
   float diameter = 100.0f;
   RectShape a_rect;
+  RectShape[] n_rects = new RectShape[10000];
   PVector position;
   float marginPercent;
-
 
   public static void main(String args[]) {
     PApplet.main(new String[] { "shapeAgent.TestLibrary" });
   }
 
-
   public void settings() {
-    size(500, 500, P2D);
-    // fullScreen(P2D);
-  }
+    // size(500, 500, P2D);
+    fullScreen(FX2D);
 
+  }
 
   public void setup() {
     rectMode(CENTER);
 
-    a_rect = new RectShape(this,0,0,80,80);
-    a_rect.enableHover(true);
+//    for (int i = 0; i < n_rects.length; i++) {
+//      n_rects[i] = new RectShape(this, 0, 0, random(10, 120), random(10, 120));
+//      n_rects[i].enableHover(true);
+//    }
     
+    for (int i = 0; i < n_rects.length; i++) {
+      n_rects[i] = new RectShape(this, 0, 0, 2, 2);
+      n_rects[i].enableHover(true);
+    }
+
     marginPercent = 20 / 100;
   }
-
 
   public void draw() {
     background(0);
     
-    if (a_rect.finishedTranslating) {
-      position = new PVector(random(marginPercent * width,(1 - marginPercent) * width),
-          random(marginPercent * height,(1 - marginPercent) * height));
+    for (int i = 0 ; i < n_rects.length ; i++) {
+      if (mousePressed) {
+        n_rects[i].finishedTranslating = true;
+        n_rects[i].translate(mouseX,mouseY,100,5);
+      }
+      else if (n_rects[i].finishedTranslating) {
+          position = new PVector(random(marginPercent * width,(1 - marginPercent) * width),
+              random(marginPercent * height,(1 - marginPercent) * height));
+          n_rects[i].translate(position, (int)random(60,250), 4);
+        }
       
-      a_rect.translate(position, 20, 4);
+      if (n_rects[i].finishedRotating) {
+        float rotateDeg = random(-35,35);
+        
+        n_rects[i].rotate(rotateDeg, 20, 4);
+      }
+      
+      if (n_rects[i].finishedFill) {
+        int fillColor = color(random(255),random(255),random(255));
+        
+        n_rects[i].setFillColor(fillColor, 120, 4);
+      }
+      
+      
+      n_rects[i].draw();
     }
     
-    if (a_rect.finishedRotating) {
-      float rotateDeg = random(-35,35);
-      
-      a_rect.rotate(rotateDeg, 25, 4);
-    }
-    
-    if (a_rect.finishedFill) {
-      int fillColor = color(random(255),random(255),random(255));
-      
-      a_rect.setFillColor(fillColor, 15, 5);
-    }
-
-    a_rect.draw();
-
     // Frame rate display
     textSize(32);
     fill(255);
     text(frameRate, 5, 30);
-  }
-
-
-  public void mousePressed() {
-    if (mouseButton == LEFT) {
-      a_rect.perpetualRotate(2, true);
-    }
-    if (mouseButton == RIGHT) {
-      a_rect.disablePerpetualRotate();
-    }
   }
 }
